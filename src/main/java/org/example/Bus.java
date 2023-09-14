10df82d900c9096cec924cc00045bd35ca08286a
Tales Aguiar <talesdd@outlook.com>
2023-09-14 14:13:17 -0300

    third commit - need to implement clients

--- src/main/java/org/example/Bus.java
+++ src/main/java/org/example/Bus.java
@@ -1,4 +1,65 @@
 package org.example;
 
+import lombok.Getter;
+
+import java.io.*;
+import java.net.*;
+import java.util.Random;
+import java.util.Scanner;
+
+@Getter
 public class Bus {
+
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("localhost", 1234);
+        System.out.println("Connection succeed");
+
+        Thread threadSend = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Available to send");
+
+                    PrintStream output = new PrintStream(socket.getOutputStream());
+                    Random random = new Random();
+
+                    int randomNumber = random.nextInt(101);
+                    output.println(randomNumber);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread threadReceiveAnswer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Receiving data from server");
+
+                    Scanner answer = new Scanner(socket.getInputStream());
+
+                    while (answer.hasNextLine()) {
+                        String line = answer.nextLine();
+                        System.out.println(line);
+                    }
+
+                    answer.close();
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        threadReceiveAnswer.start();
+        threadSend.start();
+
+        threadSend.join();
+
+        System.out.println("Client socket closing!");
+
+        socket.close();
+    }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -18,19 +22,39 @@ public class Server {
         System.out.println("Starting server");
         this.server = new ServerSocket(serverPort);
         this.threadPool = Executors.newCachedThreadPool();
+        this.running = new AtomicBoolean(true);
     }
 
-    public void runServer() throws IOException{
-        while(this.running.get()){
+    public void runServer() throws IOException {
+        while (this.running.get()) {
             try {
                 Socket socket = this.server.accept();
                 System.out.println("New client on port: " + socket.getPort());
 
-                NumberGenerator numberGenerator = new NumberGenerator();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String clientInput;
 
-                this.threadPool.execute(numberGenerator);
-            } catch (SocketException e){
-                System.out.println("SocketException, still running? " + this.running);
+                while ((clientInput = reader.readLine()) != null) {
+                    try {
+                        int busNumber = Integer.parseInt(clientInput);
+
+                        // Send the random number to the client
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                        out.println("Random Number: " + busNumber);
+
+                        System.out.println("Received: " + busNumber);
+
+                        // Example to send the manipulated number back to the client if needed
+                        // PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                        // out.println("Manipulated Number: " + manipulatedNumber);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid input from client: " + clientInput);
+                    }
+                }
+
+                socket.close();
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
             }
         }
     }
@@ -39,6 +63,11 @@ public class Server {
         this.running.set(false);
         this.threadPool.shutdown();
         this.server.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.runServer();
     }
 
 }
